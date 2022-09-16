97063109b89ff4acef638f5eb12de99fad9369f5
zhiyongzhou0125 <112611091+zhiyongzhou0125@users.noreply.github.com>
2022-09-16 10:38:35 +0200

    Added code to download data sets

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,5 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1/dataset1.csv')
+#download calendar file for lisbon
+download.file("http://data.insideairbnb.com/portugal/lisbon/lisbon/2022-06-10/data/calendar.csv.gz", "D:/OneDrive/Tilburg/MMA/DPWM/projects/team project/raw data/lisbon.csv.gz")
 
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
-
+#download calendar file for rio de janeiro
+download.file("http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-06-20/data/calendar.csv.gz", "D:/OneDrive/Tilburg/MMA/DPWM/projects/team project/raw data/rio.csv.gz")
