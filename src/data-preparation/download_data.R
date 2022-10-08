f16a3ca2ffbaa7f5d6bd1df81946d407d00fd0d4
MevludinAdemovic <112542168+MevludinAdemovic@users.noreply.github.com>
2022-10-08 19:42:32 +0200

    Update download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,5 +1,16 @@
-#download calendar file for lisbon
-download.file("http://data.insideairbnb.com/portugal/lisbon/lisbon/2022-06-10/data/calendar.csv.gz", "D:/OneDrive/Tilburg/MMA/DPWM/projects/team project/raw data/lisbon.csv.gz")
+urls = c("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/france/ile-de-france/paris/2022-09-09/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/czech-republic/prague/prague/2022-09-16/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/portugal/lisbon/lisbon/2022-09-13/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-09-21/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/mexico/df/mexico-city/2022-09-22/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/chile/rm/santiago/2022-09-22/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/argentina/ciudad-autónoma-de-buenos-aires/buenos-aires/2022-09-22/data/calendar.csv.gz")
 
-#download calendar file for rio de janeiro
-download.file("http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-06-20/data/calendar.csv.gz", "D:/OneDrive/Tilburg/MMA/DPWM/projects/team project/raw data/rio.csv.gz")
+
+for (url in urls) {
+  filename = paste0(gsub('[^a-z]', '', url), '.csv') 
+  filename = gsub('httpdatainsideairbnbcom', '', filename)
+  filename = gsub('datacalendarcsvgz', '', filename)
+  download.file(url, destfile = filename) 
+}
