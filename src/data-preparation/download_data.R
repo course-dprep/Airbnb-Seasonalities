ae2ae98bf39e6215b7baa3be2764492ff67154d4
MevludinAdemovic <112542168+MevludinAdemovic@users.noreply.github.com>
2022-10-10 14:36:51 +0200

    Update download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -2,10 +2,10 @@ urls = c("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2
          "http://data.insideairbnb.com/france/ile-de-france/paris/2022-09-09/data/calendar.csv.gz",
          "http://data.insideairbnb.com/czech-republic/prague/prague/2022-09-16/data/calendar.csv.gz",
          "http://data.insideairbnb.com/portugal/lisbon/lisbon/2022-09-13/data/calendar.csv.gz",
-         "http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-09-21/data/calendar.csv.gz",
-         "http://data.insideairbnb.com/mexico/df/mexico-city/2022-09-22/data/calendar.csv.gz",
-         "http://data.insideairbnb.com/chile/rm/santiago/2022-09-22/data/calendar.csv.gz",
-         "http://data.insideairbnb.com/argentina/ciudad-autónoma-de-buenos-aires/buenos-aires/2022-09-22/data/calendar.csv.gz")
+         "http://data.insideairbnb.com/australia/vic/melbourne/2022-09-09/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/australia/nsw/northern-rivers/2022-09-14/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/australia/nsw/sydney/2022-09-09/data/calendar.csv.gz",
+         "http://data.insideairbnb.com/australia/wa/western-australia/2022-09-20/data/calendar.csv.gz")
 
 
 for (url in urls) {
