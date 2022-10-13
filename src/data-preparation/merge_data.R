3405a07a31308e9f68a8d9d9cec5e9af3d40b2f8
MevludinAdemovic <112542168+MevludinAdemovic@users.noreply.github.com>
2022-10-13 17:09:50 +0200

    Update merge_data.R

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,20 +1,21 @@
-#MERGE THE TWO DATA FRAMES----------------------------------------
+#MERGE NORTH AND SOUTH DATA FRAMES----------------------------------------
 merged_cities_north_south <- rbind(merged_cities_north, merged_cities_south)
 View(merged_cities_north_south)
 
-#Mapping the "season" variable into numeric values which allows for analyses to be performed 
+# Mapping the "season" variable into numeric values which allows for analyses to be performed 
 mapping <- c("winter" = 1, "spring" = 2, "summer" = 3, "autumn" = 4)
 merged_cities_north_south$seasons_numeric<- mapping[merged_cities_north_south$seasons]
 
-#dummy coding the "hemisphere" variable 
+# Dummy coding of the "hemisphere" variable 
 install.packages("fastDummies")
 library(fastDummies)
 merged_cities_north_south <- dummy_cols(merged_cities_north_south, select_columns = 'Hemisphere')
 
+#MAKING SEPERATE DATA FRAMES FOR SEASONS AND MERGE THEM
 # Autumn
 amsterdam_autumn <- subset(merged_cities_north_south, City == 'Amsterdam' & seasons == 'autumn')
 lisbon_autumn <- subset(merged_cities_north_south, City == 'Lisbon' & seasons == 'autumn')
-prague_autumn <- subset(merged_cities_north_south, City == 'Prague' & seasons == 'autumn')
+ghent_autumn <- subset(merged_cities_north_south, City == 'Ghent' & seasons == 'autumn')
 vienna_autumn <- subset(merged_cities_north_south, City == 'Vienna' & seasons == 'autumn')
 melbourne_autumn <- subset(merged_cities_north_south, City == 'Melbourne' & seasons == 'autumn')
 northernrivers_autumn <- subset(merged_cities_north_south, City == 'Northern Rivers' & seasons == "autumn")
@@ -22,13 +23,13 @@ sydney_autumn <- subset(merged_cities_north_south, City == 'Sydney' & seasons ==
 westernaus_autumn <- subset(merged_cities_north_south, City == 'Western Australia' & seasons == 'autumn')
 
 # Merge autumn
-merged_cities_autumn <- rbind(amsterdam_autumn, lisbon_autumn, prague_autumn, vienna_autumn, melbourne_autumn, northernrivers_autumn, sydney_autumn, westernaus_autumn)
+merged_cities_autumn <- rbind(amsterdam_autumn, lisbon_autumn, ghent_autumn, vienna_autumn, melbourne_autumn, northernrivers_autumn, sydney_autumn, westernaus_autumn)
 View(merged_cities_autumn)
 
 # Spring
 amsterdam_spring <- subset(merged_cities_north_south, City == 'Amsterdam' & seasons == 'spring')
 lisbon_spring <- subset(merged_cities_north_south, City == 'Lisbon' & seasons == 'spring')
-prague_spring <- subset(merged_cities_north_south, City == 'Prague' & seasons == 'spring')
+ghent_spring <- subset(merged_cities_north_south, City == 'Ghent' & seasons == 'spring')
 vienna_spring <- subset(merged_cities_north_south, City == 'Vienna' & seasons == 'spring')
 melbourne_spring <- subset(merged_cities_north_south, City == 'Melbourne' & seasons == 'spring')
 northernrivers_spring <- subset(merged_cities_north_south, City == 'Northern Rivers' & seasons == "spring")
@@ -36,13 +37,13 @@ sydney_spring <- subset(merged_cities_north_south, City == 'Sydney' & seasons ==
 westernaus_spring <- subset(merged_cities_north_south, City == 'Western Australia' & seasons == 'spring')
 
 # Merge spring
-merged_cities_spring <- rbind(amsterdam_spring, lisbon_spring, prague_spring, vienna_spring, melbourne_spring, northernrivers_spring, sydney_spring, westernaus_spring)
+merged_cities_spring <- rbind(amsterdam_spring, lisbon_spring, ghent_spring, vienna_spring, melbourne_spring, northernrivers_spring, sydney_spring, westernaus_spring)
 View(merged_cities_spring)
 
 # Summer
 amsterdam_summer <- subset(merged_cities_north_south, City == 'Amsterdam' & seasons == 'summer')
 lisbon_summer <- subset(merged_cities_north_south, City == 'Lisbon' & seasons == 'summer')
-prague_summer <- subset(merged_cities_north_south, City == 'Prague' & seasons == 'summer')
+ghent_summer <- subset(merged_cities_north_south, City == 'Ghent' & seasons == 'summer')
 vienna_summer <- subset(merged_cities_north_south, City == 'Vienna' & seasons == 'summer')
 melbourne_summer <- subset(merged_cities_north_south, City == 'Melbourne' & seasons == 'summer')
 northernrivers_summer <- subset(merged_cities_north_south, City == 'Northern Rivers' & seasons == "summer")
@@ -50,13 +51,13 @@ sydney_summer <- subset(merged_cities_north_south, City == 'Sydney' & seasons ==
 westernaus_summer <- subset(merged_cities_north_south, City == 'Western Australia' & seasons == 'summer')
 
 # Merge summer
-merged_cities_summer <- rbind(amsterdam_summer, lisbon_summer, prague_summer, vienna_summer, melbourne_summer, northernrivers_summer, sydney_summer, westernaus_summer)
+merged_cities_summer <- rbind(amsterdam_summer, lisbon_summer, ghent_summer, vienna_summer, melbourne_summer, northernrivers_summer, sydney_summer, westernaus_summer)
 View(merged_cities_summer)
 
 # Winter
 amsterdam_winter <- subset(merged_cities_north_south, City == 'Amsterdam' & seasons == 'winter')
 lisbon_winter <- subset(merged_cities_north_south, City == 'Lisbon' & seasons == 'winter')
-prague_winter <- subset(merged_cities_north_south, City == 'Prague' & seasons == 'winter')
+ghent_winter <- subset(merged_cities_north_south, City == 'Ghent' & seasons == 'winter')
 vienna_winter <- subset(merged_cities_north_south, City == 'Vienna' & seasons == 'winter')
 melbourne_winter <- subset(merged_cities_north_south, City == 'Melbourne' & seasons == 'winter')
 northernrivers_winter <- subset(merged_cities_north_south, City == 'Northern Rivers' & seasons == "winter")
@@ -64,7 +65,7 @@ sydney_winter <- subset(merged_cities_north_south, City == 'Sydney' & seasons ==
 westernaus_winter <- subset(merged_cities_north_south, City == 'Western Australia' & seasons == 'winter')
 
 # Merge Winter
-merged_cities_winter <- rbind(amsterdam_winter, lisbon_winter, prague_winter, vienna_winter, melbourne_winter, northernrivers_winter, sydney_winter, westernaus_winter)
+merged_cities_winter <- rbind(amsterdam_winter, lisbon_winter, ghent_winter, vienna_winter, melbourne_winter, northernrivers_winter, sydney_winter, westernaus_winter)
 View(merged_cities_winter)
 
 # Merge every data frame
@@ -73,6 +74,9 @@ View(all_cities_merged)
 
 # Write csv files
 write.csv(all_cities_merged, "all_cities_merged.csv")
+
+
+
 
 
 
