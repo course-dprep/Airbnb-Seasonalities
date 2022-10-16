93b2418a836946b1ddfbd1338be2ee2ead0fd8b9
zhiyongzhou0125 <zhiyong.2468@gmail.com>
2022-10-16 15:30:09 +0200

    automated the pipeline using make

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -7,13 +7,12 @@ library(effectsize)
 library(haven)
 
 # set correct working directory
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 setwd("../../gen/data-preparation/output")
 
 # Read csv all_cities_merged
 all_cities_merged <- read_csv("all_cities_merged.csv")
 
-pdf("../../analysis/output/ANOVA_analysis.pdf")
+pdf("../../analysis/output/plot.pdf")
 
 # Make City and seasons factors
 all_cities_merged$City <- as_factor(all_cities_merged$City)
@@ -22,19 +21,20 @@ all_cities_merged$seasons <- as_factor(all_cities_merged$seasons)
 # Visualization (boxplot and barchart)
 ggplot(all_cities_merged, aes (x=price_numeric, y=seasons, fill=City)) + geom_col(position="dodge")
 ggplot(all_cities_merged, aes(x=price_numeric, y=seasons, fill=City))+geom_bar(stat="summary", fun="mean")
+dev.off()
 
 #Homoscedasticity
-leveneTest(price_numeric ~ City * seasons, all_cities_merged, center=mean)
+write.csv(leveneTest(price_numeric ~ City * seasons, all_cities_merged, center=mean), file.path('../../analysis/output', "Homoscedasticity.csv"))
 
 # Anova
 Price_numeric_aov1 <- lm(price_numeric ~ City * seasons, all_cities_merged, contrasts=list(City=contr.sum, seasons=contr.sum))
-Anova(Price_numeric_aov1, type=3)
+write.csv(Anova(Price_numeric_aov1, type=3), file.path('../../analysis/output', "ANOVA.csv"))
 
 # Multiple comparisons
-emmeans(Price_numeric_aov1, pairwise ~ City * seasons, adjust="bonferroni")
+write.csv(emmeans(Price_numeric_aov1, pairwise ~ City * seasons, adjust="bonferroni"), file.path('../../analysis/output', "Multiple comparions.csv"))
 
 # Partial eta squared
-eta_squared(Anova(Price_numeric_aov1, type=3), ci=0.95, partial = TRUE)
+write.csv(eta_squared(Anova(Price_numeric_aov1, type=3), ci=0.95, partial = TRUE), file.path('../../analysis/output', "Partial eta squared.csv"))
 
-dev.off()
+
 
--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -5,7 +5,6 @@ library(tidyverse)
 library(fastDummies)
 
 # Set the correct working directory
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 setwd("../../gen/data-preparation/input")
 
 merged_cities_north <- read.csv("merged_cities_north.csv")
